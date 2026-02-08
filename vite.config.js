4ceacf15e2ecf7585f5e9bd2678cd2f5357a27e7
akash soni <akashsoni9102@gmail.com>
2026-02-09 00:02:14 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000'|| "https://ms-test-backend-7qdaq3ru1-akashsoni9102s-projects.vercel.app",
+        changeOrigin: true,
+      },
+    },
+  },
+});
